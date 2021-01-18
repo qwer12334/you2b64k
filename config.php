d23ab3ff86383b99bc60bbdf2248623ec6f66de1
qwer12334 <59679467+qwer12334@users.noreply.github.com>
2021-01-18 15:53:12 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+define('APIKEY', 'AIzaSyBQSmijuR6Jb40_9V8zlgpKQWFrMJsw534');
+define('TITLENAME', 'x');
+define('SITE_NAME', 'x');
+define('NAME', 'mvc');
+define('PASSWORD', '9527aA/*-');
+define('LOGINHTML', '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="Login.css"/>
+</head>
+<body>
+<center>
+    <p id="login">
+    <h1>µÇÂ½</h1>
+    <form method="post" style="margin:0px auto;">
+        <input type="text" required="required" placeholder="ÓÃ»§Ãû" name="name"></input>
+        <br>
+        <input type="password" required="required" placeholder="ÃÜÂë" name="password"></input>
+        <button class="but" type="submit">µÇÂ¼</button>
+    </form>
+    </p>
+</center>
+</body>
+</html>');
+?>
